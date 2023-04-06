17192075ade848f787b9fa88e572264184454a26
Gai <gaiyizhi@gmail.com>
2023-04-06 21:20:07 +0900

    add pop-up windows, password generator func, and copy generated password to clipboard

--- day29/password_manager/main.py
+++ day29/password_manager/main.py
@@ -1,13 +1,44 @@
 from tkinter import *
+from tkinter import messagebox
+from random import choice, randint, shuffle
+import pyperclip
 
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def generate_password():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v',
+               'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q',
+               'R',
+               'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    password_letters = [choice(letters) for _ in range(randint(8, 10))]
+    password_symbols = [choice(symbols) for _ in range(randint(2, 4))]
+    password_numbers = [choice(numbers) for _ in range(randint(2, 4))]
+    password_list = password_letters + password_symbols + password_numbers
+    shuffle(password_list)
+
+    password = "".join(password_list)
+    password_entry.insert(0, password)
+    pyperclip.copy(password)
+
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def write_data():
     website = website_entry.get()
     email = user_name_entry.get()
     password = password_entry.get()
+
+    if len(website) == 0 or len(email) == 0 or len(password) == 0:
+        messagebox.showinfo(title="Oops", message="Please don't leave any field empty!")
+        return
+
+    is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {email} "
+                                                          f"\nPassword: {password} \nIs it ok to save?")
+    if not is_ok:
+        return
 
     with open("data.txt", "a") as f:
         save_content = f"{website} | {email} | {password}\n"
@@ -46,7 +77,7 @@ password_label.grid(column=0, row=3)
 password_entry = Entry(width=21)
 password_entry.grid(column=1, row=3)
 
-generate_button = Button(text="Generate Password", width=10)
+generate_button = Button(text="Generate Password", width=10, command=generate_password)
 generate_button.grid(column=2, row=3)
 
 add_button = Button(text="Add", width=35, command=write_data)

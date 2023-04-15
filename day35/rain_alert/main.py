12ac8d5903e2a79f305512e01641b501c9e15ae7
Gai <gaiyizhi@gmail.com>
2023-04-16 00:05:49 +0900

    use .env file to save secret info.

--- day35/rain_alert/main.py
+++ day35/rain_alert/main.py
@@ -1,16 +1,21 @@
 import requests
 import config
 from twilio.rest import Client
+import os
+from dotenv import load_dotenv
 
+load_dotenv("/Users/yizhigai/Desktop/python_environment_variables/.env")
+
+OWM_Endpoint = "https://api.openweathermap.org/data/2.5/forecast"
 parameters = {
-    "lat": config.my_lat,
-    "lon": config.my_long,
-    "appid": config.api_key,
+    "lat": float(os.getenv("MY_LAT")),
+    "lon": float(os.getenv("MY_LONG")),
+    "appid": os.getenv("OWM_API_KEY"),
     "units": "metric",
     "cnt": 30
 }
 
-response = requests.get(url=config.OWM_Endpoint, params=parameters)
+response = requests.get(url=OWM_Endpoint, params=parameters)
 response.raise_for_status()
 weather_data = response.json()["list"]
 
@@ -30,11 +35,11 @@ for weather_code in weather:
 #     print("it will rain")
 
 if will_rain:
-    client = Client(config.account_sid, config.auth_token)
+    client = Client(os.getenv("ACCOUNT_SID"), os.getenv("AUTH_TOKEN"))
     message = client.messages \
         .create(
         body="It's going to rain today. Remember to bring an ☔︎.",
-        from_=config.from_phone_number,
-        to=config.to_phone_number
+        from_=os.getenv("FROM_PHONE_NUMBER"),
+        to=os.getenv("TO_PHONE_NUMBER")
     )
     print(message.status)
